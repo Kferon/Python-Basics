7935ebf02c695c218e3c652aac4f2c43ee5cef89
Egor Klimenko <67512701+Kferon@users.noreply.github.com>
2020-07-13 18:15:37 +0300

    Delete Exercise6(a).py

--- Exercise6(a).py
+++
@@ -1,8 +0,0 @@
-from itertools import count
-number = 0
-check = True
-n = int(input('Введите количество целых чисел, которое хотите сгенерировать: '))
-for i in count(int(input('Введите число, с которого начать генерировать целые числа: '))):
-        if number < n:
-            print(i)
-            number += 1
